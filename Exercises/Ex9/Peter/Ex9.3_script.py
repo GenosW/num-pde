84ae6708b630ad4d3a8257771b60dc9a18c14a15
GenosW <peter.a.holzner@student.tuwien.ac.at>
2020-05-19 16:38:49 +0200

    Ex9.3

--- Exercises/Ex9/Peter/Ex9.3_script.py
+++ Exercises/Ex9/Peter/Ex9.3_script.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 
 # Some constants and functions
 f = 1
-ud = CoefficientFunction(x) 
+ud = CoefficientFunction(x)
 g = CoefficientFunction(y)
 alpha = 5
 
@@ -26,6 +26,7 @@ Robin = "top|right"
 # Runtimes
 runtimes = {"primal": -1, "mixed": -1}
 
+
 def primal(mesh, order=2, draw=False, rt=False):
     FESp = H1(mesh, order=order, dirichlet=Dirichlet)
     up, vp = FESp.TnT()
@@ -33,10 +34,10 @@ def primal(mesh, order=2, draw=False, rt=False):
     # Forms
     ap = BilinearForm(FESp)
     #                           + Bilinear Robin-Term
-    ap += grad(up)*grad(vp)*dx  + alpha*up*vp*ds(definedon=mesh.Boundaries(Robin))
+    ap += grad(up) * grad(vp) * dx + alpha * up * vp * ds(definedon=mesh.Boundaries(Robin))
 
     fp = LinearForm(FESp)
-    fp += f*vp*dx + g*vp*ds(definedon=mesh.Boundaries(Neumann+"|"+Robin))
+    fp += f * vp * dx + g * vp * ds(definedon=mesh.Boundaries(Neumann + "|" + Robin))
 
     with TaskManager():
         ap.Assemble()
@@ -44,7 +45,8 @@ def primal(mesh, order=2, draw=False, rt=False):
 
     # Gridfunction
     gfup = GridFunction(FESp, "u_primal")
-    gfup.Set(ud, BND) # BND =^ definedon=mesh.Boundaries("bottom|right|top|left")
+    gfup.Set(ud,
+             BND)  # BND =^ definedon=mesh.Boundaries("bottom|right|top|left")
 
     # Need to take of the Dirichlet BC
     with TaskManager():
@@ -62,31 +64,35 @@ def primal(mesh, order=2, draw=False, rt=False):
     flux = CoefficientFunction(grad(gfup))
 
     if draw:
-        Draw (gfup)
-        Draw (flux, mesh, "flux_primal")
+        Draw(gfup)
+        Draw(flux, mesh, "flux_primal")
         internal.visoptions.autoscale = False
     return gfup, flux
 
+
 def mixed(mesh, order_flux=2, draw=False, rt=False):
     # Setup the mixed method
-    V = HDiv(mesh, order=order_flux, dirichlet=Neumann) # "Neumann is the new Dirichlet" 
-    Q = L2(mesh, order=order_flux-1)
+    V = HDiv(mesh, order=order_flux,
+             dirichlet=Neumann)  # "Neumann is the new Dirichlet"
+    Q = L2(mesh, order=order_flux - 1)
 
-    FESm = FESpace([V,Q]) # Compound space
+    FESm = FESpace([V, Q])  # Compound space
     TnTm = FESm.TnT()
     (sigma, u), (tau, v) = TnTm
-     # Setup the special function "normal" 
+    # Setup the special function "normal"
     normal = specialcf.normal(mesh.dim)
 
     # Set up forms again
     am = BilinearForm(FESm)
     #      a(sig,tau)+ b(sig,v)     + b(tau,u)       + bilinear Robin-Term
-    am += (sigma*tau + div(sigma)*v + div(tau)*u)*dx + (1/alpha*(tau.Trace()*normal)*(sigma.Trace()*normal))*ds(definedon=mesh.Boundaries(Robin))
+    am += (sigma * tau + div(sigma) * v + div(tau) * u) * dx
+    am += (1 / alpha * (tau.Trace() * normal) * (sigma.Trace() * normal)) * ds(definedon=mesh.Boundaries(Robin))
     am.Assemble()
 
     fm = LinearForm(FESm)
     #     source  + linear Dirichlet-term                                            + linear Robin-term
-    fm += -f*v*dx + ud*(tau.Trace()*normal)*ds(definedon=mesh.Boundaries(Dirichlet)) + g/alpha*(tau.Trace()*normal)*ds(definedon=mesh.Boundaries(Robin)) 
+    fm += -f * v * dx + ud * (tau.Trace() * normal) * ds(definedon=mesh.Boundaries(Dirichlet)) 
+    fm += g / alpha * (tau.Trace() * normal) * ds(definedon=mesh.Boundaries(Robin))
     fm.Assemble()
 
     # Now on to the actual problem
@@ -94,7 +100,7 @@ def mixed(mesh, order_flux=2, draw=False, rt=False):
     gfsigma, gfum = gfm.components
     # Remember g from before? :D
     # It was defined as scalar valued --> "need to bring it into the vector world"
-    gfsigma.Set(normal*g, BND) 
+    gfsigma.Set(normal * g, BND)
     with TaskManager():
         start_m = time_ns()
         # fm.vec.data -= am.mat * gfm.vec
@@ -106,46 +112,48 @@ def mixed(mesh, order_flux=2, draw=False, rt=False):
             runtimes["mixed"] = round(delta_m, 3)
 
     if draw:
-        Draw (gfsigma, mesh, "flux_mixed")
-        Draw (gfum, mesh, "u_mixed")
+        Draw(gfsigma, mesh, "flux_mixed")
+        Draw(gfum, mesh, "u_mixed")
         internal.visoptions.autoscale = False
     return gfm
 
 
 hs = [0.5, 0.2, 0.1, 0.08, 0.05, 0.025, 0.01]
+
+
 def compare_PvM(h_list=hs, order=2, ask=True):
     errors_flux = []
     errors_u = []
-    for h, hnext in zip(h_list, h_list[1:]+["None"]):
-        print("#"*40)
+    for h, hnext in zip(h_list, h_list[1:] + ["None"]):
+        print("#" * 40)
         mesh = Mesh(unit_square.GenerateMesh(maxh=h))
         print("h:", h)
 
         # Solve the problems
         #order = 1
         gfup, flux = primal(mesh, order=order, draw=False)
-        gfm = mixed(mesh, order_flux=order+1, draw=False)
+        gfm = mixed(mesh, order_flux=order + 1, draw=False)
         gfsigma, gfum = gfm.components
 
         # Errors
         u_diff = gfup - gfum
-        err_u = sqrt(Integrate( (u_diff)*(u_diff), mesh))
+        err_u = sqrt(Integrate((u_diff) * (u_diff), mesh))
         errors_u.append(err_u)
-        print ("err_u:", err_u)
+        print("err_u:", err_u)
         flux_diff = grad(gfup) - gfsigma
-        err_flux = sqrt(Integrate(flux_diff*flux_diff, mesh))
+        err_flux = sqrt(Integrate(flux_diff * flux_diff, mesh))
         errors_flux.append(err_flux)
-        print ("err_flux:", err_flux)
-        
-        Draw (gfup, mesh, "u_primal")
-        Draw (flux, mesh, "flux_primal")
-        Draw (gfum, mesh, "u_mixed")
-        Draw (gfsigma, mesh, "flux_mixed")
-        Draw (u_diff, mesh, "u_diff")
-        Draw (flux_diff, mesh, "flux_diff")
+        print("err_flux:", err_flux)
+
         #internal.visoptions.linear = True
         #internal.visoptions.autoscale = False
         if ask:
+            Draw(gfup, mesh, "u_primal")
+            Draw(flux, mesh, "flux_primal")
+            Draw(gfum, mesh, "u_mixed")
+            Draw(gfsigma, mesh, "flux_mixed")
+            Draw(u_diff, mesh, "u_diff")
+            Draw(flux_diff, mesh, "flux_diff")
             if not (hnext == "None"):
                 print(f"Next h={hnext}")
             stop = input("Stop? (y/n): ")
@@ -156,15 +164,18 @@ def compare_PvM(h_list=hs, order=2, ask=True):
 
 hs = [0.5, 0.2, 0.1, 0.08, 0.05, 0.025, 0.01]
 
-fig = plt.figure(figsize=(10,8))
-orders = [1,2,3]
+fig = plt.figure(figsize=(10, 8))
+orders = [1, 2, 3]
 styles = ["x", "o", "s"]
 for order, style in zip(orders, styles):
     print(f"Order= {order}")
     errors_u, errors_flux = compare_PvM(h_list=hs, order=order, ask=True)
     hs_used = hs[0:int(len(errors_u))]
-    plt.semilogy(hs_used, errors_u, "-"+style, label=f"err(u)_o={order}")
-    plt.semilogy(hs_used, errors_flux, "--"+style, label=f"err(flux)_o={order}")
+    plt.semilogy(hs_used, errors_u, "-" + style, label=f"err(u)_o={order}")
+    plt.semilogy(hs_used,
+                 errors_flux,
+                 "--" + style,
+                 label=f"err(flux)_o={order}")
 plt.grid()
 plt.legend()
 pic = f"D:/Studium/Code/num-pde/Exercises/Ex9/Peter/Ex9.3_O=[{orders[0]}-{orders[-1]}].png"
